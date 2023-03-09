408bd6086314f3a2984bf6e34af60888012ba6b6
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-10 00:37:02 +0300

    Created login.php

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+    if(isset($_SESSION['unique_id'])){ // if user is logged in
+        header("location: users.php");
+    }
+?>
+
+<?php include_once "header.php";  ?>
+<body oncontextmenu="return false;">
+    <div class="wrapper">
+        <section class="form login">
+            <header>Realtime Chat App</header>
+            <form action="#">
+                <div class="error-txt"></div>
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" name="email" placeholder="Enter your email">
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Enter your password">
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field button">
+                    <input type="submit" value="Continue to Chat">
+                </div>
+            </form>
+            <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+        </section>
+    </div>
+
+<script src="javascript/script.js"></script>
+<script src="javascript/login.js"></script>
+
+</body>
+</html>
