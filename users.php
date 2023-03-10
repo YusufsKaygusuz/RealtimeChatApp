f2a430f87fa6fc49909bfc99b5a026ce0f54a5cb
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-10 19:40:34 +0300

    Update users.php

--- users.php
+++ users.php
@@ -15,7 +15,7 @@
             <header>
 
                 <?php
-                include_once "php/config.php";
+                include_once "php/config.php"; // config dosyasını dahil ediyorum
                 //şimdi oturumu kullanarak mevcut oturum açmış kullanıcının tüm verilerini seçeceğim
                     $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
                     if(mysqli_num_rows($sql) > 0)
@@ -23,7 +23,7 @@
                         $row = mysqli_fetch_assoc($sql);
                     }
                 ?>
-
+            <!-- Kullanıcı bilgilerini gösteren bölüm -->
             <div class="content">
                 <img src="php/images/<?php echo $row['img'] ?>">
                 <div class="details">
@@ -31,13 +31,16 @@
                     <p><?php echo $row['status'] ?></p>
                 </div>
             </div>
+                <!-- Logout butonu -->
             <a href="php/logout.php?logout_id=<?php echo $row['unique_id'] ?>" class="logout">Logout</a>
             </header>
+             <!-- Arama kutusu -->
             <div class="search">
                 <span class="text">Select an user to start chat</span>
                 <input type="text" placeholder="Enter name to search...">
                 <button><i class="fas fa-search"></i></button>
             </div>
+            <!-- Kullanıcı listesi -->
             <div class="users-list">
             </div>
         </section>
