19b10c37fcb4ee31f91aa01397fe27b672311ea3
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-10 23:07:46 +0300

    Update data.php

--- php/data.php
+++ php/data.php
@@ -1,13 +1,14 @@
 <?php
     while($row = mysqli_fetch_assoc($sql))
     {
-            $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
-            OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id}
-            OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+        $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = ? OR outgoing_msg_id = ?) AND (outgoing_msg_id = ? OR incoming_msg_id = ?) ORDER BY msg_id DESC LIMIT 1";
+        $stmt = mysqli_prepare($conn, $sql2);
+        mysqli_stmt_bind_param($stmt, 'iiii', $row['unique_id'], $row['unique_id'], $outgoing_id, $outgoing_id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $row2 = mysqli_fetch_assoc($result);
 
-        $query2 = mysqli_query($conn, $sql2);
-        $row2 = mysqli_fetch_assoc($query2);
-        if(mysqli_num_rows($query2) > 0){
+        if(mysqli_num_rows($result) > 0){
             $result = $row2['msg'];
         }
         else{
@@ -17,25 +18,30 @@
         (strlen($result) > 26) ? $msg = substr($result, 0, 26).'...' : $msg = $result;
         // sizi ekliyorum: oturum açma kimliği msg gönderirse mesajdan önce metin yazın
         if($row2){
-            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = ""; 
+            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
         }
         else
         {
             $you = ""; 
         }
         // Kullanıcının onine olup olmadığının kontrolü
+        /*
+        HTML kodunu oluştururken, kullanıcı girdilerini doğrulayın ve güvenli bir şekilde işleyin.
+        Örneğin, htmlspecialchars() fonksiyonu kullanarak kullanıcı girdilerindeki HTML öğelerini
+        ve özel karakterleri güvenli bir şekilde kaçırabilirsiniz.
+        */
         ($row['status'] == "Offline now") ? $offline = "offline" : $offline ="";
 
-        $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
-                <div class="content">
-                <img src="php/images/'. $row['img'] . '" alt="">
-                <div class="details">
-                    <span>'. $row['fname'] . " " . $row['lname']  .'</span>
-                    <p>'. $you . $msg .'</p>
-                </div>
-                </div>
-                <div class="status-dot '.$offline.'"><i class="fas fa-circle"></i></div>
-            </a>';
+        $output .= '<a href="chat.php?user_id='.htmlspecialchars($row['unique_id'], ENT_QUOTES).'">
+            <div class="content">
+            <img src="php/images/'.htmlspecialchars($row['img'], ENT_QUOTES).'" alt="">
+            <div class="details">
+                <span>'.htmlspecialchars($row['fname'], ENT_QUOTES).' '.htmlspecialchars($row['lname'], ENT_QUOTES).'</span>
+                <p>'.htmlspecialchars($you . $msg, ENT_QUOTES).'</p>
+            </div>
+            </div>
+            <div class="status-dot '.htmlspecialchars($offline, ENT_QUOTES).'"><i class="fas fa-circle"></i></div>
+        </a>';
     }
-
 ?>
+
