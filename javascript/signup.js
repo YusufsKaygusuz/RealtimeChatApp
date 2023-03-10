14e59ddc61a48e9203241cecb6e129a3d19c9a1a
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-10 19:50:13 +0300

    Update signup.js

--- javascript/signup.js
+++ javascript/signup.js
@@ -6,6 +6,13 @@ form.onsubmit = (e)=>{
     e.preventDefault(); // form göndermeyi engelleme
 }
 
+// Kayıt ol butonuna tıklandığında AJAX kullanarak form verilerini PHP'ye gönderiyoruz.
+// XMLHTTP nesnesi oluşturuyoruz ve 'php/signup.php' adresine POST isteği gönderiyoruz.
+// Sunucudan cevap gelinceye kadar sayfa yenilenmeden işlem devam eder.
+// Gelen cevap 200 ise, cevap metni 'data' değişkeninde tutulur ve 'success' ise kullanıcı sayfasına yönlendirilir.
+// Aksi takdirde, hata mesajı görüntülenir.
+// Son olarak, form verileri, FormData nesnesi kullanılarak alınır ve XMLHTTP nesnesi ile PHP'ye gönderilir.
+
 continueBtn.onclick = ()=>{
     // Ajax'a başlıyoruz
     let xhr = new XMLHttpRequest(); // XML nesnesi oluşturma
