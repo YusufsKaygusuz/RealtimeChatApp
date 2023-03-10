bd80198ceb5738e41db552323782faedc5b8f177
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-10 23:23:13 +0300

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,22 +1,42 @@
 <?php
     session_start();
     include_once "config.php";
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
     
     if(!empty($email) && !empty($password))
     {
-        // Girilen mail ile parola verileri database ile eşleşip eşlenmediğinin kontrolü
-        $sql = mysqli_query($conn, "SELECT * FROM users WHERE email =  '{$email}' AND password = '{$password}' ");
-        if(mysqli_num_rows($sql) >0) // kullanıcı kimlik bilgileri eşleşirse
+        // Password Hashing
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Prepared Statements
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if($result->num_rows > 0)
         {
-            $row = mysqli_fetch_assoc($sql);
-            $status = "Active now";
-            // kullanıcı oturumu başarıyla açılırsa, kullanıcı durumu şimdi etkin olarak güncelleniyor
-            $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
-            if($sql2){
-                $_SESSION['unique_id'] = $row['unique_id'];
-                echo "success";
+            $row = $result->fetch_assoc();
+            if(password_verify($password, $row['password']))
+            {
+                $status = "Active now";
+                // Sanitize User Input
+                $unique_id = htmlspecialchars($row['unique_id']);
+
+                // Prepared Statements
+                $stmt = $conn->prepare("UPDATE users SET status = ? WHERE unique_id = ?");
+                $stmt->bind_param("ss", $status, $unique_id);
+                $stmt->execute();
+
+                if($stmt->affected_rows == 1){
+                    $_SESSION['unique_id'] = $unique_id;
+                    echo "success";
+                }
+            }
+            else{
+                echo "Email or Password is incorrect!";
             }
         }
         else{
