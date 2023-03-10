2a20fb17447821b10ea7b590b7151485b5afc89a
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-10 19:48:25 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,10 @@
 const pswrdField = document.querySelector(".form input[type='password']"),
 toggleBtn = document.querySelector(".form .field i");
 
+// Bu kod, kullanıcının şifre alanındaki metni gizlemek veya göstermek için bir toggle düğmesi oluşturur.
+// Kullanıcı düğmeye tıkladığında, şifre alanındaki metin ya gizlenir ya da gösterilir.
+// Bu, kullanıcıların şifrelerini gizleyerek girdikleri bir giriş formu için kullanılabilir.
+
 toggleBtn.onclick = ()=> {
     if(pswrdField.type == "password"){
         pswrdField.type = "text";
