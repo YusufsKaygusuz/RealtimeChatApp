e78fc3110bbf1c2c9e572ec5e74dd1245e90a9f2
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-10 23:05:53 +0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,12 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","chat");
-    if(!$conn){
-        echo "Datebase connected" . mysqli_connect_error();
-    }
+
+include "connection.php";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(mysqli_connect_errno()){
+    // hata oluştuğunda özel bir mesaj göster
+    echo "Database connection failed: " . mysqli_connect_error();
+}
 
 ?>
