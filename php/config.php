8f934b6a4684ff48dd4ba91221e68b44c2e02aa2
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-16 12:13:31 +0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,11 +1,14 @@
 <?php
 
+// veritabanı bağlantı detayları "connection.php" adlı harici bir dosyadan dahil edilir
 include "connection.php";
 
+// mysqli_connect işlevini kullanarak veritabanına bir bağlantı oluşturun
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
+// veritabanına bağlantının başarılı olup olmadığını kontrol edin
 if(mysqli_connect_errno()){
-    // hata oluştuğunda özel bir mesaj göster
+    // bağlantı sırasında bir hata oluşursa, echo ifadesini kullanarak özel bir hata mesajı göster
     echo "Database connection failed: " . mysqli_connect_error();
 }
 
