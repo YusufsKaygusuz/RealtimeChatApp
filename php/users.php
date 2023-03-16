68e8232738be35f8c620b02a12669ff3b74f65fe
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-16 13:15:20 +0300

    Update users.php

--- php/users.php
+++ php/users.php
@@ -1,17 +1,17 @@
 <?php
-    session_start();
-    include_once "config.php";
-    $outgoing_id = $_SESSION['unique_id'];
-    $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id}");
-    $output = "";
+    session_start(); // Session başlatılır.
+    include_once "config.php";  // Veritabanı bağlantı bilgileri içeren config dosyası dahil edilir.
+    $outgoing_id = $_SESSION['unique_id']; // Aktif kullanıcının benzersiz kimliği alınır.
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id}"); // Kullanıcılar tablosundan aktif kullanıcının haricindeki kullanıcılar sorgulanır.
+    $output = ""; // Boş bir değişken tanımlanır.
 
-    if(mysqli_num_rows($sql) == 1){
-        $output .= "No users are available to chat";
+    if(mysqli_num_rows($sql) == 1){  // Eğer sorgudan sadece bir sonuç dönerse "No users are available to chat" mesajı oluşturulur.
+        $output .= "No users are available to chat"; //"Mesajlaşabileceğiniz kullanıcı bulunamadı.";
     }
-    else if(mysqli_num_rows($sql) > 0) 
+    else if(mysqli_num_rows($sql) > 0) // Eğer sorgudan birden fazla sonuç dönerse "data.php" dosyası dahil edilir.
     {
         include "data.php";
     }
-    echo $output;
+    echo $output;  // Oluşturulan çıktı echo ile yayılır.
 
 ?>
