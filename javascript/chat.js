6ba5897ccfc110b58b5d4f13c1476e9dac1cc105
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-16 12:01:54 +0300

    Update chat.js

--- javascript/chat.js
+++ javascript/chat.js
@@ -8,6 +8,7 @@ engellemek için kullanılıyor. form değişkeni, .typing-area sınıfı olan H
 form.onsubmit olayı, form gönderildiğinde çalıştırılacak fonksiyonu belirtiyor.
 Bu fonksiyonda e.preventDefault() kullanılarak, formun varsayılan işlemini durdurarak sayfanın yenilenmesi önlenebiliyor.
 */
+
 form.onsubmit = (e) =>{
     e.preventDefault();
 }
@@ -38,37 +39,24 @@ chatBox.onmouseleave = () =>{
 
 function escapeHtml(unsafe) {
     return String(unsafe)
-            .replace('<script>', "Kural dışı mesaj ")
-           .replace('/script', "Kural dışı mesaj ")
-           .replace('alert()', "Kural dışı mesaj ")
-           .replace('script', "Kural dışı mesaj ")
-           .replace('<script>', "Kural dışı mesaj ")
-           .replace('</h2', "Kural dışı mesaj ")
-           .replace('</h2', "Kural dışı mesaj ")
-           .replace('</h3', "Kural dışı mesaj ")
-           .replace('</h4', "Kural dışı mesaj ")
-           .replace('</h5', "Kural dışı mesaj ")
-           .replace('</h6', "Kural dışı mesaj ")
-           .replace('document.', "Kural dışı mesaj ")
-           .replace('();', "Kural dışı mesaj ")
-           .replace('.querySelector', "Kural dışı mesaj ");
+           .replace('argo', "Kural dışı mesaj ");
 }
 
 // Get Yöntemini kullanacağız çünkü verileri göndermek için almamız gerekiyor
 setInterval( ()=>{
-    // Ajax isteği başlatılıyor
+    // Ajax'a başlıyoruz
     let xhr = new XMLHttpRequest(); // XML nesnesi oluşturma
     xhr.open("POST", "php/get-chat.php", true);
     xhr.onreadystatechange = ()=>{
         if(xhr.readyState === 4){
             if(xhr.status === 200){
                 let data = xhr.response;
-                data = escapeHtml(data); // Veriyi HTML kodlarından temizleme
-                data = document.createElement("div").innerText = data; // Veriyi HTML elementi olarak oluşturma
-                chatBox.innerHTML = data; // Sohbet kutusunun içeriğini güncelleme
+                data = escapeHtml(data);
+                data = document.createElement("div").innerText = data;
+                chatBox.innerHTML = data;
                 if(!chatBox.classList.contains("active"))
                 {
-                    scrollToBottom(); // Eğer sohbet kutusu aktif değilse, en altına kaydırma
+                    scrollToBottom();
                 }
             }
         }
@@ -80,12 +68,4 @@ setInterval( ()=>{
 
 function scrollToBottom(){
     chatBox.scrollTop = chatBox.scrollHeight;
-}
-
-// Kullanıcı girdilerini kontrol eden bir fonksiyon
-function validateData(data){
-    // verileri filtreleyerek veya doğrulayarak güvenli hale getirin
-    // örneğin, özel karakterleri filtreleyebilirsiniz
-    // yalnızca metin verilerine izin vermek için aşağıdaki örneği kullanabilirsiniz
-    return data.replace(/<\/?[^>]+(>|$)/g, '');
 }
