2ed814abb7ab5aaa71bef417ddfb03c4e2eb5cc1
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2023-03-16 11:41:43 +0300

    Update users.js

--- javascript/users.js
+++ javascript/users.js
@@ -1,51 +1,60 @@
 const searchBar = document.querySelector(".users .search input"),
-searchBtn = document.querySelector(".users .search button"),
-userList = document.querySelector(".users .users-list");
+    searchBtn = document.querySelector(".users .search button"),
+    userList = document.querySelector(".users .users-list");
 
-searchBtn.onclick = ()=>{
+searchBtn.onclick = () => {
     searchBar.classList.toggle("active");
     searchBar.focus();
     searchBtn.classList.toggle("active");
     searchBar.value = "";
 }
-// HTML'de oluşturduğumuz arama çubuğunun değerini değişiklikleri dinlemek için tanımlıyoruz
-searchBar.onkeyup = ()=>{
-    let searchTerm = searchBar.value; // Arama çubuğundaki değeri alıyoruz
-    if (searchTerm != ""){
-        searchBar.classList.add("active"); // Eğer değer varsa arama çubuğuna "active" class'ını ekliyoruz
+
+searchBar.onkeyup = () => {
+    let searchTerm = searchBar.value.trim();
+    if (searchTerm != "") {
+        searchBar.classList.add("active");
     }
-    else{
-        searchBar.classList.remove("active"); // Değer yoksa "active" class'ını çıkarıyoruz
+    else {
+        searchBar.classList.remove("active");
     }
-    // Yeni bir XMLHttpRequest nesnesi oluşturuyoruz
-    let xhr = new XMLHttpRequest(); 
-    xhr.open("POST", "php/search.php", true); // PHP dosyasına istek göndermek için HTTP POST isteği yapıyoruz
-    xhr.onreadystatechange  = ()=>{
-        if(xhr.readyState === 4){ // İstek tamamlandı mı diye kontrol ediyoruz
-            if(xhr.status === 200){ // HTTP yanıt kodu 200 ise (yani işlem başarılı olduysa)
-                let data = xhr.response; // PHP dosyasından gelen verileri alıyoruz
-                userList.innerHTML = data; // Verileri kullanıcı listesi HTML elementine ekliyoruz
+    // Ajax'a başlıyoruz
+    let xhr = new XMLHttpRequest(); // XML nesnesi oluşturma
+    xhr.open("POST", "php/search.php", true);
+    
+    // CSRF koruması
+    let csrfToken = document.querySelector('meta[name="csrf-token"]');
+    if (csrfToken) {
+      xhr.setRequestHeader("X-CSRF-Token", csrfToken.getAttribute('content'));
+    }
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                userList.innerHTML = data;
             }
         }
     }
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // POST isteği ile gönderilen verilerin türünü belirtiyoruz
-    xhr.send("searchTerm=" + searchTerm); // Arama çubuğundaki değeri POST isteği ile gönderiyoruz
+    // SQL enjeksiyonuna karşı koruma
+    searchTerm = searchTerm.replace(/'/g, "\\'");
+    searchTerm = searchTerm.replace(/"/g, '\\"');
+    let params = "searchTerm=" + encodeURI(searchTerm);
+
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+    //xhr.send("searchTerm=" + searchTerm); // ajax ile php dosyasına kullanıcı arama terimi gönderme
 }
 
-// Aşağıdaki kod, her 500 milisaniyede bir users.php dosyasına GET isteği göndererek,
-// kullanıcıların listesini alıp userList elementinin içeriğini güncelliyor.
-// Eğer arama kutusu aktif değilse (searchBar elementi active class'ını içermiyorsa),
-// kullanıcı listesini güncelliyor. Bu sayede, anlık olarak kullanıcıların
-// eklenip çıkarılması durumunda da kullanıcı listesi güncel tutuluyor.
-setInterval( ()=>{
+// Get Yöntemini kullanacağız çünkü verileri göndermek için almamız gerekiyor
+setInterval(() => {
     // Ajax'a başlıyoruz
     let xhr = new XMLHttpRequest(); // XML nesnesi oluşturma
     xhr.open("GET", "php/users.php", true);
-    xhr.onreadystatechange  = ()=>{
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            if(xhr.status === 200){
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
                 let data = xhr.response;
-                if(!searchBar.classList.contains("active")){
+                if (!searchBar.classList.contains("active")) {
                     userList.innerHTML = data
                 }
             }
